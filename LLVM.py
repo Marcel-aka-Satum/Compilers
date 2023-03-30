b762eb15142135d81eeb451f188b5b48b313f1d3
Marcel <wilczynskimarceli@gmail.com>
2023-03-31 00:45:35 +0200

    fixed pointers in LLVM

--- LLVM.py
+++ LLVM.py
@@ -5,14 +5,29 @@ from AST import *
 #• Compiler: x86-64 clang 16.0.0
 #• Compiler options: -S -emit-llvm
 class LLVM:
-    def __init__(self) -> None:
+    def __init__(self, argv) -> None:
         self.output = ""
+        self.argv = argv
 
     def generate_LLVM(self, table):
         #loop through symbolTable
         for dict1 in table:
             #loop through dicts of table (can have multiple scopes)
             for key in dict1:
+                #if the value is a node we need to traverse it to get its value
+                if(isinstance(dict1[key][2], AST)):
+                    if((dict1[key][1][0] == 'pointer' or dict1[key][1][0] == 'const pointer') and dict1[key][0] == 'int'):
+                        parent = self.traverse_node(dict1[key][2])
+                        self.output += f"@{key} = dso_local global ptr @{parent}, allign 8 \n"
+                        continue
+                    elif((dict1[key][1][0] == 'pointer' or dict1[key][1][0] == 'const pointer') and dict1[key][0] == 'float'):
+                        parent = self.traverse_node(dict1[key][2])
+                        self.output += f"@{key} = dso_local global ptr @{parent}, allign 8 \n"
+                        continue
+                    elif((dict1[key][1][0] == 'pointer' or dict1[key][1][0] == 'const pointer') and dict1[key][0] == 'char'):
+                        parent = self.traverse_node(dict1[key][2])
+                        self.output += f"@{key} = dso_local global ptr @{parent}, allign 8 \n"
+                        continue
                 if(dict1[key][0] == 'int'):
                     if(dict1[key][1] == 'const'):
                         self.output += f"@{key} = dso_local constant i32 {dict1[key][2]}, allign 4\n"
@@ -28,5 +43,14 @@ class LLVM:
                         self.output += f"@{key} = dso_local consant i8 {ord(dict1[key][2])}, allign 1\n"
                     else:
                         self.output += f"@{key} = dso_local global i8 {ord(dict1[key][2])}, allign 1\n"
-        print(self.output)
-                
+        self.print_ll(self.argv)
+
+    def traverse_node(self, node):
+        if node.node.getRuleName() == "referenceID":
+            parent = value = node.children[1].children[0].node.getRuleName()
+        return parent
+    
+    def print_ll(self, argv):
+        argv += ".ll"
+        file = open(argv, "w")
+        file.write(self.output)
